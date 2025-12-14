45afccc5239d822f2c0751f6ce79cf6046abcc79
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-12-13 21:23:24 -0600

    refactor search input

--- frontend/stockmaster-client/src/app/features/user/catalog/components/product-search/product-search.ts
+++ frontend/stockmaster-client/src/app/features/user/catalog/components/product-search/product-search.ts
@@ -1,10 +1,11 @@
 import { Component, output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
+import { SearchInput } from '../../../../../shared/ui/inputs/search-input/search-input';
 
 @Component({
   selector: 'app-product-search',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, SearchInput],
   templateUrl: './product-search.html',
   styleUrls: ['./product-search.css'],
 })
