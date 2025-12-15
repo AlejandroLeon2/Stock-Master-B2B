269e9e3a2123460d488d96fb6cf9e3dc5fac7441
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-12-15 13:27:31 -0600

    Update catalog service with pagination

--- frontend/stockmaster-client/src/app/features/user/catalog/components/product-search/product-search.ts
+++ frontend/stockmaster-client/src/app/features/user/catalog/components/product-search/product-search.ts
@@ -1,5 +1,6 @@
-import { Component, output } from '@angular/core';
+import { Component, effect, inject, input, output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 import { SearchInput } from '../../../../../shared/ui/inputs/search-input/search-input';
 
@@ -10,19 +11,36 @@ import { SearchInput } from '../../../../../shared/ui/inputs/search-input/search
   styleUrls: ['./product-search.css'],
 })
 export class ProductSearch {
-  searchControl = new FormControl('');
+  term = input<string>('');
+  searchControl = new FormControl(this.term());
 
   searchChange = output<string>();
 
   private destroy$ = new Subject<void>();
 
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
   constructor() {
+    effect(() => {
+      // Update the form control value without emitting an event
+      this.searchControl.setValue(this.term(), { emitEvent: false });
+    });
     this.searchControl.valueChanges
       .pipe(debounceTime(500), distinctUntilChanged(), takeUntil(this.destroy$))
       .subscribe((term) => {
-        this.searchChange.emit(term?.trim() || '');
+        const searchTerm = term?.trim() || '';
+        this.searchChange.emit(searchTerm);
+        // Update the URL with the new page parameter
+
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: { search: searchTerm || null },
+          queryParamsHandling: 'replace', // This will replace other query parameters
+        });
       });
   }
+
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
--- frontend/stockmaster-client/src/app/features/user/catalog/services/catalog.service.ts
+++ frontend/stockmaster-client/src/app/features/user/catalog/services/catalog.service.ts
@@ -1,11 +1,10 @@
 import { inject, Injectable, OnDestroy, signal } from '@angular/core';
 import {
-  BehaviorSubject,
   catchError,
   debounceTime,
-  distinctUntilChanged,
   filter,
   map,
+  of,
   Subject,
   switchMap,
   takeUntil,
@@ -14,25 +13,49 @@ import {
 import { ApiService } from '../../../../core/http/api.service';
 import { Product } from '../../../../core/models/product.model';
 
+interface SearchProductsResponse {
+  products: Product[];
+  metadata: { count: number; pages: number };
+}
+
+const defaultResponse: SearchProductsResponse = {
+  products: [],
+  metadata: { count: 0, pages: 0 },
+};
+
 @Injectable({
   providedIn: 'root',
 })
 export class CatalogService implements OnDestroy {
   private api = inject(ApiService);
 
-  private search$ = new BehaviorSubject<string>('');
+  private search$ = new Subject<string>();
   private destroy$ = new Subject<void>();
 
   products = signal<Product[]>([]);
   loading = signal(false);
   error = signal<string | null>(null);
 
+  term = signal<string>('');
+  page = signal<number>(1);
+  metadata = signal<SearchProductsResponse['metadata']>({
+    count: 0,
+    pages: 0,
+  });
+
   constructor() {
     this.initSearchFlow();
   }
 
   searchProducts(term: string) {
+    this.page.set(1);
     this.search$.next(term);
+  }
+
+  updateSearch({ search, page }: { search: string; page: number }) {
+    this.term.set(search);
+    this.page.set(page);
+    this.search$.next(search);
   }
 
   private initSearchFlow() {
@@ -40,34 +63,40 @@ export class CatalogService implements OnDestroy {
       .pipe(
         debounceTime(300),
         map((term) => term.trim()),
-        distinctUntilChanged(),
         filter((term) => term.length >= 2 || term.length === 0),
-        tap(() => {
+        tap((term) => {
+          if (term !== this.term()) {
+            this.page.set(1);
+          }
+          this.term.set(term);
           this.loading.set(true);
           this.error.set(null);
         }),
         switchMap((term) =>
           this.api
-            .get<Product[]>('/products', {
+            .get<SearchProductsResponse>('/products', {
               params: {
                 search: term,
-                page: 1,
-                limit: 10,
+                page: this.page(),
+                limit: 4,
               },
             })
             .pipe(
-              map((response) => response.data || []),
+              map((response) => {
+                return response.data || defaultResponse;
+              }),
               catchError((err) => {
                 console.error(err);
                 this.error.set('Error al buscar productos');
-                return [[] as Product[]];
+                return of(defaultResponse);
               })
             )
         ),
         takeUntil(this.destroy$)
       )
-      .subscribe((products) => {
-        this.products.set(products);
+      .subscribe((data) => {
+        this.products.set(data.products || []);
+        this.metadata.set(data.metadata);
         this.loading.set(false);
       });
   }
